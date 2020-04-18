ec350967622b4acf3b59229bdb9aef7ea5b082b0
Jared <49449253+jdcollier136@users.noreply.github.com>
2020-04-18 09:36:20 -0600

    Added HangmanGame file to repository

---
+++ HangmanGame.java
@@ -0,0 +1,32 @@
+/**
+ * A program to play a game of Hangman.
+ */
+public class HangmanGame {
+
+    /**
+     * Creates a new HangmanGame object with a default word list.
+     */
+    public HangmanGame() {
+        // TODO: FILL ME IN!
+    }
+
+    /**
+     * Creates a new HangmanGame object, based on the given list of words.
+     * 
+     * @param wordList The list of words that the game chooses from.
+     */
+    public HangmanGame(String[] wordList) {
+        // TODO: FILL ME IN!
+    }
+
+    /**
+     * A method that plays Hangman. This method plays a full game of Hangman. When
+     * the game is over (win or lose), the player is prompted to play again, which
+     * restarts the game. The method returns when the player chooses not to play
+     * again.
+     */
+    public void playGame() {
+        // TODO: FILL ME IN!
+    }
+
+} // HangmanGame
