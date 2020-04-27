f83e0a62622f15f760432d720018538e29b03401
Jared <49449253+jdcollier136@users.noreply.github.com>
2020-04-26 20:04:25 -0600

    update

--- HangmanGameDemo.java
+++ HangmanGameDemo.java
@@ -6,15 +6,14 @@ import java.util.*;
 
 public class HangmanGameDemo {
     public static void main(String[] args) {
-        
         ArrayList<String> testingWordList = new ArrayList<String>();
         testingWordList.add("computer");
         testingWordList.add("doge");
         testingWordList.add("class");
 
         // Test array object
-        HangmanGame game = new HangmanGame(testingWordList);
-        //game.playGame();
+        // HangmanGame game = new HangmanGame(testingWordList);
+        // game.playGame();
 
         
         // Final ArrayList Object
